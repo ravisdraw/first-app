1086180c37fc538b80a1ff80e2c22564f57688db
ravisdraw <ravikumarrk1118@gmail.com>
2022-12-22 17:27:10 +0530

    public api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import ConnectPublicAPI from './components/ConnectPublicAPI'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Let's Rock it Ravi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConnectPublicAPI/>
   );
 }
 
