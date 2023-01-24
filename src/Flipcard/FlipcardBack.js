ea610dd4eb19afa4e77891d4602a9bbfcf5e29d9
ravisdraw <ravikumarrk1118@gmail.com>
2023-01-25 01:33:07 +0530

    bug fix

--- src/Flipcard/FlipcardBack.js
+++ src/Flipcard/FlipcardBack.js
@@ -15,7 +15,7 @@ function FlipcardBack(props) {
 
     const keyPoints = backData.keyPoints;
 
-    const texts = useMemo(() => console.log(keyPoints), [keyPoints]);
+    // const texts = useMemo(() => console.log(keyPoints), [keyPoints]);
     // const [currentText, setCurrentText] = useState(texts[0]);
     const [index, setIndex] = useState(0); // Initialize the index state to 0
 
@@ -23,19 +23,20 @@ function FlipcardBack(props) {
         function loop() {
             setTimeout(() => {
                 setIndex(index + 1); // Increment the index by 1 every 5 seconds
-                if (index === texts.length - 1) setIndex(0);
+                const len = keyPoints.length;
+                if (index === len - 1) setIndex(0);
                 // loop();
             }, 5000);
         }
         loop();
-    }, [keyPoints, index, texts.length]);
+    }, [keyPoints, index]);
 
 
     return (
         <div className='FlipcardBack'>
             <div className="Projectdescription">
                 <p>{backData.sourceCode}</p>
-                <p className='topicCovered'>{texts[index]}</p>
+                <p className='topicCovered'>{keyPoints[index]}</p>
             </div>
             <div className="projectButtonWrapper">
                 <div className="projectButtons" >
